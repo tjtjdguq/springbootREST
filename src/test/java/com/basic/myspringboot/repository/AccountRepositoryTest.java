894c0c08817b7ecbe4d899a0cde43daa7010c0cd
myjungwu <baek.myungsuk@gmail.com>
2022-04-19 17:34:19 +0900

    testclass add

--- src/test/java/com/basic/myspringboot/repository/AccountRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/repository/AccountRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.basic.myspringboot.repository;
 
+import com.basic.myspringboot.entity.Account;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +10,17 @@ public class AccountRepositoryTest {
     @Autowired
     AccountRepository accountRepository;
 
-    @Test
+    @Test  //ctrl + shift + f10
     public void crud() throws Exception {
-
+        //1.insert
+        //Account 객체생성
+        Account account = new Account();
+        //값을 저장
+        account.setUsername("spring2");
+        account.setPassword("1234");
+        //등록요청
+        Account savedAccount = accountRepository.save(account);
+        System.out.println("ID = " + savedAccount.getId());
+        System.out.println("username = " + savedAccount.getUsername());
     }
 }
