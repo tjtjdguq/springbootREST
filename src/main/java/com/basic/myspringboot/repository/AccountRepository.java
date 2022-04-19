bac809e8e313d3c2e566ff760bc685a1840bc92f
myjungwu <baek.myungsuk@gmail.com>
2022-04-19 16:20:13 +0900

    entity,repository add

--- src/main/java/com/basic/myspringboot/repository/AccountRepository.java
+++ src/main/java/com/basic/myspringboot/repository/AccountRepository.java
@@ -1,4 +1,9 @@
 package com.basic.myspringboot.repository;
 
-public interface AccountRepository {
+import com.basic.myspringboot.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account,Long> {
+    // username로 조회하는 finder method 선언
+    Account findByUsername(String username);
 }
