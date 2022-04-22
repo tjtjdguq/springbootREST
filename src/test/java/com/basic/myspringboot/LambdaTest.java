a5d809bcace6279a38155122c64c265af85317e1
myjungwu <baek.myungsuk@gmail.com>
2022-04-22 16:34:17 +0900

    ëlambda

--- src/test/java/com/basic/myspringboot/LambdaTest.java
+++ src/test/java/com/basic/myspringboot/LambdaTest.java
@@ -1,9 +1,13 @@
 package com.basic.myspringboot;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class LambdaTest {
-    @Test
+    @Test @Disabled
     public void runnable() throws Exception {
         //1. MyRunnable 클래스 사용
         Thread t1 = new Thread(new MyRunnable());
@@ -17,8 +21,26 @@ public class LambdaTest {
             }
         });
         t2.start();
+
+        //3. Lambda Expression
+        //Runnble이 가진 run() 메서드를 재정의하는 구문을 람다식으로 표현할 수 있다.
+        Thread t3 = new Thread(() -> System.out.println("람다식으로 구현함"));
+        t3.start();
     }
 }
+@Getter
+@Setter
+@AllArgsConstructor
+class User {
+    private String name;
+    private int age;
+
+//    public User(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+}
+
 
 class MyRunnable implements Runnable{
     @Override
